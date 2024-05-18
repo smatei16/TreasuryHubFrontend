3e2693e77d7b7615bab9870ca1e38f4a8a1cc177
smatei16 <55893625+smatei16@users.noreply.github.com>
2024-05-18 16:08:02 +0300

    [TH-18] Integrated login and register without design

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,14 +1,15 @@
 import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import "./SignIn.css";
 
 export default function SignUp() {
 
-    const URL = "https://treasury-hub-backend-162bca8c9d0c.herokuapp.com";
-    // const URL = "http://localhost:8080";
-    const [firstName, setFirstName]  = useState("");
-    const [lastName, setLastName]  = useState("");
+    // const URL = "https://treasury-hub-backend-162bca8c9d0c.herokuapp.com";
+    const URL = "http://localhost:8080";
     const [email, setEmail]  = useState("");
     const [password, setPassword]  = useState("");
     const [error, setError] = useState("");
+    const navigate = useNavigate();
 
     const handleEmail = (e) => {
         setEmail(e.target.value);
@@ -18,45 +19,63 @@ export default function SignUp() {
         setPassword(e.target.value);
     }
 
-    const handleSubmit = (e) => {
+    async function handleSubmit(e) {
         e.preventDefault();
         const userData = {email: email, password: password};
         const requestBody = JSON.stringify(userData);
-        fetch(`${URL}/login`, {
-            // credentials: 'same-origin',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            method: "POST",
-            body: requestBody
-        })
-            .then((response) => response.text())
-            .then((result) => {
-            console.log(result);
-        })
-    };
+        try {
+            const response = await fetch(`${URL}/login`, {
+                // credentials: 'same-origin',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                method: "POST",
+                body: requestBody
+            });
+            if(response.ok) {
+                const data = await response.json();
+                const token = data.token;
+                console.log(token);
+                localStorage.setItem('token', token);
+                navigate("/dashboard");
+            } else if(response.status === 403) {
+                setError("Incorrect email/password");
+            } else {
+                const errorData = await response.text();
+                console.log(errorData);
+            }
+        } catch (errorMessage) {
+            console.error(errorMessage);
+        }
+
+        //TODO - update backend to send error message instead of just 403 forbidden
+        //TODO - update backend to send loginresponsedto as response
+    }
 
     return (
-        <div className="main">
-            <form>
-                <input
-                    type="email"
-                    placeholder={"Enter your email"}
-                    required
-                    onChange={handleEmail}
-                ></input>
-                <input
-                    type="password"
-                    placeholder={"Enter your password"}
-                    required
-                    onChange={handlePassword}
-                ></input>
-                <input
-                    type="submit"
-                    value="Login"
-                    onClick={handleSubmit}
-                ></input>
-            </form>
+        <div className="login-div">
+            <div className="login-text">Sign In</div>
+            <div className="login-text-underline"></div>
+            <div className="login-inputs">
+                <div className="login-input">
+                    <span className="login-input-name">Email</span>
+                    <input className="login-input-input" type="email" placeholder="Enter your email" required
+                           onChange={handleEmail}></input>
+                </div>
+                <div className="login-input">
+                    <span className="login-input-name">Password</span>
+                    <input className="login-input-input" type="password" placeholder="Enter your password" required
+                           onChange={handlePassword}></input>
+                </div>
+                <div className="login-input">
+                    <input className="login-input-submit" type="submit" value="Sign In"
+                           onClick={handleSubmit}></input>
+                </div>
+                {error &&
+                    <div className="login-input-error">
+                        <p className="login-input-error-message">{error}</p>
+                    </div>}
+            </div>
         </div>
     );
 }
